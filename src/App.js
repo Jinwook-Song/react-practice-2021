37ce71ed90e2a9643c8f8376e0fc33f739497862
jinwook-song <wlsdnr129@naver.com>
2021-11-04 18:03:43 +0900

    Movie App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((data) => {
-        setCoins(data);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id}>
-              {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} $
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
